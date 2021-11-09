db6ba74e9b187b91581efbda621e2c5adb9574b3
Kevin Loughead <kvnloughead@gmail.com>
2021-11-08 23:31:20 -0500

    adjust default app structure, by... - removing images and avatars - making HeroPost layout consistent with PostPreview layout - adding simple "Most recent post" subheader - adding divider between most recent post and other posts

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,44 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Container from '../components/container';
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPosts } from '../lib/api';
+import Head from 'next/head';
+import { BLOG_SITE_NAME, CMS_NAME } from '../lib/constants';
+import DateFormatter from '../components/date-formatter';
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>{BLOG_SITE_NAME}</title>
         </Head>
         <Container>
           <Intro />
           {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
+            <>
+              <h2>Most recent post</h2>
+              <DateFormatter dateString={heroPost.date} />
+              <HeroPost
+                title={heroPost.title}
+                coverImage={heroPost.coverImage}
+                date={heroPost.date}
+                author={heroPost.author}
+                slug={heroPost.slug}
+                excerpt={heroPost.excerpt}
+              />
+            </>
           )}
+          <div className='border-b-4 border-blue-500'></div>
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
@@ -43,9 +49,9 @@ export async function getStaticProps() {
     'author',
     'coverImage',
     'excerpt',
-  ])
+  ]);
 
   return {
     props: { allPosts },
-  }
+  };
 }
