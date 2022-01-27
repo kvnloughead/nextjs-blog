5af91f34c7f0ee601f0e18d5e43f4276d71353fd
Kevin Loughead <kvnloughead@gmail.com>
2022-01-26 21:15:11 -0500

    Make max-width wider for primary page elements

--- components/post-header.js
+++ components/post-header.js
@@ -7,7 +7,7 @@ export default function PostHeader({ title, date }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="max-w-2xl mx-auto">
+      <div className="mx-auto">
         <div className="date-text">
           <DateFormatter dateString={date} />
         </div>
